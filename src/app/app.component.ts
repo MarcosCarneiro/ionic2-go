41dd16fb7d3f7129fc6aa51fc702a47906cb901a
Marcos Carneiro Lima <marcos.lima.crn@gmail.com>
2017-01-03 22:06:42 -0300

    feat(app.component): Chama a camera através do plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,42 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
+import { CameraPreview } from 'ionic-native';
 import { HomePage } from '../pages/home/home';
 
 
 @Component({
-  templateUrl: 'app.html'
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
   rootPage = HomePage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+
       StatusBar.styleDefault();
       Splashscreen.hide();
+
+      let tapEnabled: any = false;
+      let dragEnabled: any = false;
+      let toBack: any = true;
+      let alpha = 1;
+      let rect: any = {
+        x: 0,
+        y: 0,
+        width: platform.width(),
+        height: platform.height()
+      };
+
+      CameraPreview.startCamera(
+        rect,
+        'rear',
+        tapEnabled,
+        dragEnabled,
+        toBack,
+        alpha
+      );
+      
     });
   }
 }
